0a915c3d73ace1b8d922bd0442565d312e03ea45
elahelahe <elaheshojaeehabibzadeh@gmail.com>
2024-01-05 15:51:23 +0330

    show prediction result

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // clear btn for radio btns
     var clearButton = document.querySelector('.clear-btn');
     var radioButtons = document.querySelectorAll('input[name=gender]');
 
@@ -7,4 +8,35 @@ document.addEventListener('DOMContentLoaded', function () {
             radioButton.checked = false;
         });
     });
+
+
+    // send request for prediction
+    var submitButton = document.getElementById('submit-btn');
+    var nameInput = document.getElementById('name')
+    var probOutput = document.getElementById('predicted-probability')
+    var genderOutput = document.getElementById('predicted-gender')
+
+    submitButton.addEventListener('click', function () {
+        var name = nameInput.value
+        var url = 'https://api.genderize.io/?name=' + name;
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                genderOutput.textContent = data.gender;
+                probOutput.textContent = data.probability
+
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Handle the error (e.g., display an error message to the user)
+            });
+
+    })
+
 });
